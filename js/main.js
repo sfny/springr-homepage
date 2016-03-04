62c6bfc819d5f678eccd98807b1976c00c1cacc7
sfny <sunrisefade+github@gmail.com>
2016-03-04 14:26:27 -0800

    scroll working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// smooth scroll to link + delay
 $('a[href^="#"]').on('click', function(event) {
-var target = $( $(this).attr('href') );
-  if( target.length ) {
-      event.preventDefault();
-      $('html, body').animate({
-          scrollTop: target.offset().top
-      }, 1000);
-  }
+    var target = $( $(this).attr('href') );
+    if( target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
 });
